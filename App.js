ce3eef65492ffed81bb4bbd1265b2dc4c9df87a8
aranzalazaro <163032264+aranzalazaro@users.noreply.github.com>
2024-07-16 18:50:52 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import header from"./componentes/header/header"
+console.log (header)
+
+function App() {
+  return (
+    <div className="App">
+{header()}
+
+    </div>
+  );
+}
+
+export default App;
